86ca1aa8d5f3b615a2f4eb5709bef9633f5c4d23
Arturas Zuta <arturaszuta@gmail.com>
2019-09-11 14:18:52 -0400

    Add basic dasboard using react component classes

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,12 +1,74 @@
 import React, { Component } from "react";
-
+import Loading from "./Loading";
+import Panel from "./Panel"
 import classnames from "classnames";
 
-class Dashboard extends Component {
-  render() {
-    const dashboardClasses = classnames("dashboard");
+const data = [
+  {
+    id: 1,
+    label: "Total Interviews",
+    value: 6
+  },
+  {
+    id: 2,
+    label: "Least Popular Time Slot",
+    value: "1pm"
+  },
+  {
+    id: 3,
+    label: "Most Popular Day",
+    value: "Wednesday"
+  },
+  {
+    id: 4,
+    label: "Interviews Per Day",
+    value: "2.3"
+  }
+];
 
-    return <main className={dashboardClasses} />;
+
+
+let PanelArray = [];
+
+class Dashboard extends Component {
+  
+  state = { 
+    loading: false,
+    focused: null
+  };
+
+  selectPanel(id) {
+    this.setState(prevState => ({
+      focused: prevState.focused ? null : id
+    }));
+  }
+  
+  render() {
+
+    PanelArray = data
+    .filter(
+      panel => this.state.focused === null || this.state.focused === panel.id
+      )
+      .map((element) => {
+        return <Panel
+        key={element.id} 
+        id={element.id}
+        label={element.label}
+        value={element.value}
+        onSelect={event => this.selectPanel(element.id)}
+        />
+      })
+    const dashboardClasses = classnames({ "dashboard" : true,
+    "dashboard--focused": this.state.focused
+    });
+
+    if (this.state.loading) {
+      return <Loading />;
+    }
+
+    return <main className={dashboardClasses}>
+      {PanelArray}
+      </main>;
   }
 }
 
